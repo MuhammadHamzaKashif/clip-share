5bb451720f66e060836ca6e44c60d96259a5edae
MuhammadHamzaKashif <hamzakashifkhanzada@gmail.com>
2026-08-12 04:20:20 +0500

    fix the widget test for the new layout

--- app/test/widget_test.dart
+++ app/test/widget_test.dart
@@ -2,10 +2,14 @@ import 'dart:io';
 
 import 'package:clipshare/main.dart';
 import 'package:clipshare/platform/config_store.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('home screen renders the main controls', (tester) async {
+    tester.view.physicalSize = const Size(1280, 800);
+    tester.view.devicePixelRatio = 1.0;
+    addTearDown(tester.view.reset);
     final tmp = Directory.systemTemp.createTempSync('clipshare-widget');
     await tester.pumpWidget(ClipShareApp(store: ConfigStore(tmp), startEngine: false));
     await tester.pumpAndSettle();
@@ -13,7 +17,8 @@ void main() {
     expect(find.text('ClipShare'), findsOneWidget);
     expect(find.text('Pair device'), findsOneWidget);
     expect(find.text('Start sync'), findsOneWidget);
-    expect(find.text('Recent clipboard items'), findsOneWidget);
+    expect(find.text('Recent'), findsOneWidget);
+    expect(find.text('Devices'), findsOneWidget);
     expect(find.text('history: 50 items'), findsOneWidget);
 
     tmp.deleteSync(recursive: true);
